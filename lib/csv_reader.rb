81539006885c9aabf28c7db2a32ffea60f0ea4da
David Klein <dwklein49@gmail.com>
2013-09-15 16:32:53 -0700

    Added new lines to CSVReader in a new branch

--- lib/csv_reader.rb
+++ lib/csv_reader.rb
@@ -14,9 +14,29 @@ class CSVReader
         #the following code will remove the quotes
         h.gsub('"', '')
         h.strip!
+
+        h.underscore.to_sym
       end
+    end
+    def create_hash(values)
+      h = {}
+      @headers.each_with_index do |header, i|
+        # remove new lines from the value
+        value = values[i].strip.gsub('"', '')
+        h[header] = value unless value.empty?
+      end
+      h
     end
 end
 
 class String 
   def underscore 
+    self.gsub(/::/, '/').
+    gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+    gsub(/([a-z\d])([A-Z])/,'\1_\2').
+    tr("-", "_").
+    downcase
+  end
+end
+
+
